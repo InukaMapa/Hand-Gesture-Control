8b4e21239f31fd21009c1d16b1ff26aaf7ce2a5c
Inuka_Mapa <139098767+InukaMapa@users.noreply.github.com>
2026-05-22 15:47:26 +0530

    fix some internal issue on hand detecting

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ cap = cv2.VideoCapture(0)
 
 # MediaPipe Hands
 mpHands = mp.solutions.hands
+
 hands = mpHands.Hands(
     min_detection_confidence=0.7,
     min_tracking_confidence=0.7
 )
+
 mpDraw = mp.solutions.drawing_utils
 
 # Volume setup
@@ -35,7 +37,10 @@ volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
 
-pTime =  0
+# FPS variables
+pTime = 0
+
+# Smooth volume
 smoothness = 5
 currentVol = 0
 
@@ -46,14 +51,18 @@ while True:
     if not success:
         break
 
-    # Convert image to RGB
+    # Convert BGR to RGB
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Process image
+    # Process hand detection
     results = hands.process(imgRGB)
+
+    handDetected = False
 
     # Detect hands
     if results.multi_hand_landmarks:
+
+        handDetected = True
 
         for handLms in results.multi_hand_landmarks:
 
@@ -76,6 +85,7 @@ while True:
 
                 # Thumb tip
                 if id == 4:
+
                     thumb_x, thumb_y = cx, cy
 
                     cv2.circle(
@@ -88,6 +98,7 @@ while True:
 
                 # Index finger tip
                 if id == 8:
+
                     index_x, index_y = cx, cy
 
                     cv2.circle(
@@ -98,12 +109,24 @@ while True:
                         cv2.FILLED
                     )
 
-            # Draw line between fingers
+            # Distance calculation
+            distance = math.hypot(
+                index_x - thumb_x,
+                index_y - thumb_y
+            )
+
+            # Line color
+            lineColor = (0, 255, 0)
+
+            if distance < 40:
+                lineColor = (0, 0, 255)
+
+            # Draw line
             cv2.line(
                 img,
                 (thumb_x, thumb_y),
                 (index_x, index_y),
-                (0, 255, 0),
+                lineColor,
                 3
             )
 
@@ -115,14 +138,8 @@ while True:
                 img,
                 (center_x, center_y),
                 10,
-                (0, 255, 0),
+                lineColor,
                 cv2.FILLED
-            )
-
-            # Distance calculation
-            distance = math.hypot(
-                index_x - thumb_x,
-                index_y - thumb_y
             )
 
             # Convert distance to volume
@@ -132,8 +149,13 @@ while True:
                 [minVol, maxVol]
             )
 
+            # Smooth volume
             currentVol = currentVol + (vol - currentVol) / smoothness
-            volume.SetMasterVolumeLevel(currentVol, None)
+
+            volume.SetMasterVolumeLevel(
+                currentVol,
+                None
+            )
 
             # Volume percentage
             volPercent = np.interp(
@@ -141,13 +163,15 @@ while True:
                 [20, 200],
                 [0, 100]
             )
+
+            # Volume bar
             volBar = np.interp(
-                 distance,
+                distance,
                 [20, 200],
                 [400, 150]
-        )
+            )
 
-            # Show volume text
+            # Volume text
             cv2.putText(
                 img,
                 f'Volume: {int(volPercent)}%',
@@ -157,26 +181,14 @@ while True:
                 (0, 255, 0),
                 3
             )
-            cTime = time.time()
-            fps = 1 / (cTime - pTime)
-            pTime = cTime
 
-            cv2.putText(
-                img,
-                f'FPS: {int(fps)}',
-                (40, 100),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 0, 0),
-                3
-           )
             # Volume bar outline
             cv2.rectangle(
-              img,
-             (50, 150),
-             (85, 400),
-            (0, 255, 0),
-             3
+                img,
+                (50, 150),
+                (85, 400),
+                (0, 255, 0),
+                3
             )
 
             # Filled volume bar
@@ -187,8 +199,43 @@ while True:
                 (0, 255, 0),
                 cv2.FILLED
             )
-            cv2.imshow("Hand Gesture Volume Control", img)
 
+    else:
+
+        cv2.putText(
+            img,
+            "No Hand Detected",
+            (40, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 255),
+            3
+        )
+
+    # FPS calculation
+    cTime = time.time()
+
+    fps = 1 / (cTime - pTime)
+
+    pTime = cTime
+
+    cv2.putText(
+        img,
+        f'FPS: {int(fps)}',
+        (40, 100),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 0, 0),
+        3
+    )
+
+    # Show output
+    cv2.imshow(
+        "Hand Gesture Volume Control",
+        img
+    )
+
+    # Exit key
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
